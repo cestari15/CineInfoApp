4d5b727ca47bc4e5fba90e6dd589a1f5de6e41d7
cestari15 <rafacjacobs1502@gmail.com>
2024-05-23 15:56:06 -0300

    Commit

--- src/Listagem.tsx
+++ src/Listagem.tsx
@@ -1,129 +1,170 @@
+
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity, ScrollView } from "react-native";
-import Footer from "./screens/Footer";
-import FooterAdm from "./screens/FooterAdm";
+import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity, TextInput } from "react-native";
+
 import { useNavigation } from "@react-navigation/native";
+import FooterAdm from "./screens/FooterAdm";
 
 
 
-const ListagemFilmes: React.FC = () => {
-    const [filmes, setFilmes] = useState<Filme[]>([]);
+interface Filme {
+  id: string;
+  titulo: string;
+  diretor: string;
+  genero: string;
+  dt_lancamento: string;
+  sinopse: string;
+  elenco: string;
+  classificacao: string;
+  plataformas: string;
+  duracao: string;
+}
 
-    useEffect(() => {
-        ListagemFilmes();
-    }, []);
 
-    const ListagemFilmes = async () => {
-        try {
-            const response = await axios.get('http://10.137.11.214:8000/api/filmes/listagem');
-            if (response.status === 200) {
-                setFilmes(response.data.data);
-                console.log(response)
-                console.log(filmes);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+const ListagemF: React.FC = () => {
+  const [pesquisa, setPesquisa] = useState<string>("");
+  const [filmes, setFilmes] = useState<Filme[]>([]);
+  const [elementVisible, setElementVisible] = useState<string | null>(null);
+
+  useEffect(() => {
+    ListagemFilmes();
+  }, []);
+
+
+// Filme
+const ListagemFilmes = async () => {
+  try {
+    if(pesquisa != ""){
+    const response = await axios.get('http://10.137.11.213/api/filmes/pesquisar/'+pesquisa);
+    setFilmes(response.data.data);
+    } else {
+      const response = await axios.get('http://10.137.11.213/api/filmes/listagem');
+    setFilmes(response.data.data);
     }
-    const navigation = useNavigation();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    const renderItem = ({ item }: { item: Filme }) => (
-        <View style={styles.item} key={item.id}>
-            <TouchableOpacity><Image style={styles.image} source={require('./assets/images/filme2.jpg')} /></TouchableOpacity>
-            <Text style={styles.textNome}>{item.titulo}</Text>
-
-            <TouchableOpacity onPress={() => navigation.navigate('EditarFilmes', { item })}>
-                <Image source={require('./assets/images/lapis.png')} style={styles.editarImage} />
-            </TouchableOpacity>
-
-        </View>
-    );
-
-    return (
+const Delete = async (id: number) => {
+  axios.delete('http://10.137.11.213/api/filmes/delete/' + id).then(function (response) {ListagemFilmes();}
+  ).catch(function (error) {
+  console.log(error)
+  ListagemFilmes();
+})
+}
 
 
-        <View style={styles.container}>
+const renderItem = ({ item }: { item: Filme }) => (
+  <View style={styles.item} key={item.id}>
+    <TouchableOpacity onPress={() =>
+        setElementVisible(elementVisible === item.id ? null : item.id)} > 
+    <Text style={styles.nameText}>{item.titulo}</Text>
+    <Text style={styles.text}>{item.genero}</Text>
+    <Text style={styles.numbertext}>{item.dt_lancamento}</Text>
+    <Text style={styles.text}>{item.classificacao}</Text>
+    {elementVisible === item.id && ( 
+      <View >
+    <Text style={styles.text}>{item.diretor} </Text>
+    <Text style={styles.text}>{item.sinopse}</Text>
+    <Text style={styles.text}>{item.elenco}</Text>
+    <Text style={styles.text}>{item.plataformas}</Text>
+    <Text style={styles.numbertext}>{item.duracao}</Text>
+    <TouchableOpacity onPress={() => Delete(item.id)}>
+    <Image source={require('../assets/images/trash.png')}style={styles.trash}/>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => navigation.navigate('EditarFilmes', { item })}>
+                <Image source={require('../assets/images/pen.png')} style={styles.editarImage} />
+    </TouchableOpacity>
+    </View >
+    )} 
+      </TouchableOpacity>
+  </View>
+);
 
-            <ScrollView>
+const navigation = useNavigation();
 
+  return (
+    <View style={styles.container}>
+ 
+      <View>
+        <TouchableOpacity>
+          <Image source={require('../assets/images/logo.png')} style={styles.Logo} />
+        </TouchableOpacity>
+      </View>
+     
+     <View>
+      <TextInput style={styles.pesquisa} placeholder="Pesquisar" onChangeText={setPesquisa} />
+        <TouchableOpacity onPress={ListagemFilmes}><Text>Pesquisar</Text></TouchableOpacity>
+     </View>
 
-                <View>
-                    <TouchableOpacity>
-                        <Image source={require('./assets/images/logo.png')} style={styles.Logo} />
-                    </TouchableOpacity>
-                </View>
+      <FlatList
+        data={filmes}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
 
-
-                <View>
-
-                    <FlatList
-                        data={filmes}
-                        renderItem={renderItem}
-                        keyExtractor={(item) => item.id}
-                        horizontal
-                        showsHorizontalScrollIndicator={false}
-
-
-                    />
-
-                </View>
-            </ScrollView>
-            <FooterAdm />
-        </View>
-
-    );
+    <FooterAdm/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    editarImage: {
-        height: 50,
-        width: 120
-    },
-    image: {
-        height: 100,
-        width: 201,
-        marginTop: -20,
-        borderRadius: 10
-    },
-    footer: {},
-    footerIcon: {},
     container: {
         flex: 1,
         backgroundColor: 'white'
     },
-    hIcon: {},
-    especialides: {},
-    item: {
-        backgroundColor: 'white',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        borderRadius: 20,
-        color: 'white',
-        height: 400,
-        width: 200,
-        alignItems: 'center'
-    },
-    textNome: {
-        color: 'white',
-
-
-    },
-    textIngredientes: {
-        color: 'white',
-
-    },
-    textValor: {
-        color: 'white'
-    },
-    Logo: {
-        height: 150,
-        width: 300,
-        marginTop: 20,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-    }
+    editarImage: {
+      height: 50,
+      width: 120
+  },
+  pesquisa:{
+    borderWidth:1,
+    borderRadius:20,
+    height:50,
+    width:380,
+    marginLeft:'auto',
+    marginRight:'auto'  
+  },
+  item: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: 'black',
+    marginStart: 110,
+    marginTop: 10
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  trash: {
+    height: 50,
+    width: 50,
+  },
+  button: {
+    height: 50,
+    width: 50,
+    left: 180,
+  },
+  nameText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+},
+    numbertext: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+},
+  Logo: {
+    height: 150,
+    width: 300,
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
 });
 
-export default ListagemFilmes;
+export default ListagemF;
